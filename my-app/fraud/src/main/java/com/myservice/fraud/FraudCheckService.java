bb5f2e171cd297e28a6346a5edaad7379f5c6c66
Quy Thai <quy.thai@personifyinc.com>
2023-02-07 17:06:11 +0700

    Send fraud check event to kafka

--- my-app/fraud/src/main/java/com/myservice/fraud/FraudCheckService.java
+++ my-app/fraud/src/main/java/com/myservice/fraud/FraudCheckService.java
@@ -1,9 +1,14 @@
 package com.myservice.fraud;
 
 import lombok.AllArgsConstructor;
+import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Service
@@ -18,6 +23,23 @@ public class FraudCheckService {
                         .createdAt(LocalDateTime.now())
                         .build()
         );
+
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(20))
+                .build();
+        JSONObject payload = new JSONObject();
+        payload.put("request", "fraud check request for customer " + customerId);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://KAFKA-PRODUCER/api/v1/messages/publishMessage"))
+                .timeout(Duration.ofMinutes(2))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(payload.toJSONString()))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println);
         return false;
     }
 }
